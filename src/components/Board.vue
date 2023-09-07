941b09f54a66d61f971515b716875a1ce64679cc
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-07 13:56:35 +0530

    added board

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,5 +1,42 @@
 <template>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+   <main class="flex justify-center items-center h-screen">
+    <section>
+      Game
+    </section>
+   </main>
 </template>
+
+<script setup>
+import { ref,computed } from 'vue';
+
+const player = ref("X");
+const board = ref([
+  ['','',''],
+  ['','',''],
+  ['','','']
+])
+
+//from react docs
+const CalculateWinner = (squares) =>{
+  const lines = [
+    //winning lines
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
+</script>
