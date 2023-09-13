8fff8baf37554a81e5d734743f612f36e392ec6e
rahulbabu7 <rahulbabu436@gmail.com>
2023-09-13 18:19:39 +0530

    completed Tic Tac Toe

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,15 +1,8 @@
-<template>
-   <main class="flex justify-center items-center h-screen">
-    <section>
-      Game
-    </section>
-   </main>
-</template>
 
 <script setup>
 import { ref,computed } from 'vue';
 
-const player = ref("X");
+const player = ref("X");  //settting first player as X
 const board = ref([
   ['','',''],
   ['','',''],
@@ -17,29 +10,101 @@ const board = ref([
 ])
 
 //from react docs
-const CalculateWinner = (squares) =>{
+// This function checks if there is a winner in a tic-tac-toe game.
+const CalculateWinner = (squares) => {
+  // These are the possible ways to win in tic-tac-toe. Each array represents a winning combination.
   const lines = [
-    //winning lines
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
+    [0, 1, 2],  // Top row
+    [3, 4, 5],  // Middle row
+    [6, 7, 8],  // Bottom row
+    [0, 3, 6],  // Left column
+    [1, 4, 7],  // Middle column
+    [2, 5, 8],  // Right column
+    [0, 4, 8],  // Diagonal from top-left to bottom-right
+    [2, 4, 6],  // Diagonal from top-right to bottom-left
   ];
+
+  // Now, we're going to check each of these winning combinations.
   for (let i = 0; i < lines.length; i++) {
+    // Get the three square positions (indices) we're going to check for a win.
     const [a, b, c] = lines[i];
+
+    // Check if there's a symbol (either 'X' or 'O') in the first square of the current combination (squares[a]).
+    // Then, see if the symbol in that square is the same as the symbols in the other two squares (squares[b] and squares[c]).
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      // If all three squares have the same symbol and it's not an empty square (null), we have a winner!
+      // Return the symbol of the winner ('X' or 'O').
       return squares[a];
     }
   }
+
+  // If we've checked all possible combinations and didn't find a winner, the game is not won yet.
+  // So, we return null to indicate that there is no winner at this point.
   return null;
 }
+
 //calculating winner
-const winner = computed(()=>CalculateWinner(board.value.flat())
-)
+const winner = computed(()=>CalculateWinner(board.value.flat()));
+
+const makeMove = (x,y) =>{
+  if(winner.value) return   //already found a winner
+  if(board.value[x][y] !== '')return //the square box is not empty
+
+  board.value[x][y] = player.value  //if its x turn the mark board with X
+  //if its O turn the mark the board with O
+
+  //changing the player turn
+  player.value = player.value ==='X' ? 'O' : 'X';
+  //if player is x then change it to O 
+  //if player is O the change it to X
+
+
+}
+
+//Reseting the game 
+const resetGame = ()=>{
+   player.value = "X";  //settting first player as X
+board.value = [
+  ['','',''],
+  ['','',''],
+  ['','','']
+]
+}
 
 
 </script>
+
+<template>
+<main class="pt-8 text-center dark:bg-gray-800 min-h-screen dark: text-white ">
+  <h1 class="mb-8 text-3xl font-bold uppercase">
+   Tic Tac Toe 
+  </h1>
+
+  <h3 class="text-xl mb-4">Player {{ player }}'s turn</h3>
+
+  <div class="flex flex-col items-center mb-8">
+
+    <div
+     v-for="(row,x) in board"
+     :key="x"
+     class="flex"
+    >
+    <div
+     v-for="(cell,y) in row"
+     :key="y"
+     @click="makeMove(x,y)"
+     :class="`border border-white w-20 h-20 hover:bg-gray-700 flex
+     items-center justify-center material-icons-outlined text-4xl cursor-pointer ${ cell ==='X'? 'text-pink-500' :'text-blue-500'}`"
+    >
+    {{ cell ==='X'?'X':cell ==='O' ?'O' :' ' }}
+    
+    </div>
+    </div>
+  </div>
+<h2 class="text-6xl font-bold mb-8" v-if="winner">
+  Player "{{ winner }}" Wins
+</h2>
+<button @click="resetGame" class="px-4 py-2 bg-pink-500 rounded uppercase font-bold hover:bg-pink-600 duration-300"> reset game</button>
+</main>
+
+</template>
